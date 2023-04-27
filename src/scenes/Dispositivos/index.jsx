9c6af8f5a905f1bc66edc3c7cf35146a935634ae
Alexandre <alucas@ubiwhere.com>
2023-04-27 17:08:57 +0100

    second commit

--- src/scenes/Dispositivos/index.jsx
+++ src/scenes/Dispositivos/index.jsx
@@ -1,17 +1,73 @@
-import { Box } from "@mui/material";
+import { Box, Typography, useTheme } from "@mui/material";
 import Header from "../components/Header";
+import { tokens } from "../../theme";
+import TopInfo from "../components/TopInfo";
 
-const Dispositivos = () => {
+const Dashboard = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
   return (
     <Box m="15px">
       <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Header
-          title="DISPOSITIVOS"
-          subtitle="Ligação de dispositivos externos"
-        />
+        <Header title="DASHBOARD" subtitle="Visão geral da fábrica" />
+      </Box>
+      {/* TOP INFO */}
+      <Box
+        display="grid"
+        gridTemplateColumns="repeat(15, 1fr)"
+        gridAutoFlow="140px"
+        gap="20px"
+      >
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <TopInfo title="Em produção" subtitle="20" />
+        </Box>
+
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <TopInfo title="Montagem" subtitle="20" />
+        </Box>
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <TopInfo title="Desmontagem" subtitle="20" />
+        </Box>
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <TopInfo title="Alex" subtitle="20" />
+        </Box>
+        <Box
+          gridColumn="span 3"
+          backgroundColor={colors.primary[400]}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <TopInfo title="Finalizadas" subtitle="20" />
+        </Box>
       </Box>
     </Box>
   );
 };
 
-export default Dispositivos;
+export default Dashboard;
